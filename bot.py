24f4ccefcfe3c98c2e0d517f2d1c46e1d7f586e0
swcydsqzjb-bit <swcydsqzjb@privaterelay.appleid.com>
2026-06-02 00:34:36 +0300

    Update bot.py
    fix auto mode

--- bot.py
+++ bot.py
@@ -740,8 +740,8 @@ def hazirlik_15dk_5dk_tetik_scan():
 if __name__ == "__main__":
     now = datetime.now(ZoneInfo("Europe/Istanbul"))
 
-    send_message(f"🧪 TEST | Bot çalıştı | MODE={MODE} | Saat={now.strftime('%H:%M')}")
-    send_message("🧪 AUTO BLOĞUNA GİRDİ")
+    #send_message(f"🧪 TEST | Bot çalıştı | MODE={MODE} | Saat={now.strftime('%H:%M')}")
+    #send_message("🧪 AUTO BLOĞUNA GİRDİ")
     
     if now.weekday() >= 5 and MODE != "daily":
         exit()
